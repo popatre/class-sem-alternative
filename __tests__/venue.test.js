3e7e9c3c2e4a407b4d4d20c02a04e7f93c9fbfc9
Popatre <jonny_m111@hotmail.com>
2023-10-24 17:51:12 +0100

    add sem 2 start point

--- __tests__/venue.test.js
+++ __tests__/venue.test.js
@@ -5,26 +5,7 @@ describe("Venue", () => {
         const testVenue = new Venue();
         expect(testVenue.currentlyBooked).toEqual([]);
     });
-    test(".makeBooking adds to currently booked", () => {
-        const testVenue = new Venue();
-        const John = new JohnBonham();
-        testVenue.makeBooking(John);
-        expect(testVenue.currentlyBooked[0]).toEqual(John);
-    });
-    test(".makeBooking returns currently performing musician when invoked with no arguments and musician booked", () => {
-        const consoleSpy = jest.spyOn(console, "log");
-        const testVenue = new Venue();
-        const John = new JohnBonham();
-        testVenue.makeBooking(John);
-        expect(testVenue.makeBooking()).toEqual(John);
-        expect(consoleSpy).toHaveBeenCalledWith(`John - go perform now`);
-    });
-    test(".makeBooking notifies if not performer when invoked with no arguments", () => {
-        const consoleSpy = jest.spyOn(console, "log");
-        const testVenue = new Venue();
-        testVenue.makeBooking();
-        expect(consoleSpy).toHaveBeenCalledWith("No one to go on stage");
-    });
+
     test(".isVacant- return true when no one booked to perform ", () => {
         const testVenue = new Venue();
         expect(testVenue.isVacant()).toBe(true);
@@ -35,14 +16,9 @@ describe("Venue", () => {
         testVenue.makeBooking(John);
         expect(testVenue.isVacant()).toBe(false);
     });
-    test(".checkBill - returns no show when no one booked", () => {
-        const testVenue = new Venue();
-        expect(testVenue.checkBill()).toBe("no show");
-    });
-    test(".checkBill - returns musician name booked to perform if there is someone", () => {
-        const testVenue = new Venue();
-        const John = new JohnBonham();
-        testVenue.makeBooking(John);
-        expect(testVenue.checkBill()).toBe("John");
-    });
+
+    test(".makeBooking adds performer to currently booked if no one currently booked", () => {});
+    test(".makeBooking does not add performer, if one is already booked", () => {});
+    test(".makeBooking returns currently performing musician when invoked with no arguments and musician booked", () => {});
+    test(".makeBooking notifies if there is no performer when invoked with no arguments", () => {});
 });
--- classes/venue.js
+++ classes/venue.js
@@ -3,26 +3,9 @@ class Venue {
         this.currentlyBooked = [];
     }
 
-    makeBooking(musician) {
-        if (this.isVacant()) {
-            if (!musician) {
-                console.log("No one to go on stage");
-            } else {
-                this.currentlyBooked.push(musician);
-            }
-        } else {
-            console.log(`${this.currentlyBooked[0].name} - go perform now`);
-            return this.currentlyBooked[0];
-        }
-    }
+    makeBooking() {}
     isVacant() {
         return this.currentlyBooked.length === 0;
-    }
-    checkBill() {
-        if (!this.isVacant()) {
-            return this.currentlyBooked[0].name;
-        }
-        return "no show";
     }
 }
 
